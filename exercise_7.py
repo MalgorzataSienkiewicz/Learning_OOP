169e2ad9d541792c9198ef8b5ff473b1faf60dee
Małgorzata Sienkiewicz <golszanska@gmail.com>
2024-12-06 14:02:39 +0100

    gettery, settery

---
+++ exercise_7.py
@@ -0,0 +1,26 @@
+class User:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self._name = name.upper()
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, age):
+        if age >= 0:
+            self._age = age
+        else:
+            raise ValueError
+
+user = User("Gosia", 34)
+print(f'{user.name} {user.age}')
